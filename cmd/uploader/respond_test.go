142846dfb2f7652fc2ff74ef770e7b669d779099
M. Sonntag <michael.p.sonntag@gmail.com>
2021-09-17 17:07:13 +0200

    [test/respond] Add success page test

--- cmd/uploader/respond_test.go
+++ cmd/uploader/respond_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -35,5 +38,69 @@ func TestPrepareTemplate(t *testing.T) {
 	_, err = PrepareTemplate(EmailFailTmpl)
 	if err != nil {
 		t.Fatalf("Failed to prepare 'EmailFailTmpl': %v", err)
+	}
+}
+
+func TestSuccess(t *testing.T) {
+	// test parsing valid success page content
+	pDat := &BCPoster{
+		Session:        "session",
+		AbstractNumber: "absnum",
+		Authors:        "auth",
+		Title:          "title",
+		Topic:          "topic",
+		ID:             "id",
+		UploadKey:      "upkey",
+		Abstract:       "text",
+	}
+	valDat := map[string]interface{}{
+		"UserData":          pDat,
+		"PDFPath":           "posterPath",
+		"VideoURL":          "videoURL",
+		"PosterHash":        "posterHash",
+		"supportemail":      "supportEmail",
+		"conferencepageurl": "conferencePageURL",
+	}
+	w := httptest.NewRecorder()
+	success(w, valDat)
+	if w.Result().StatusCode != 200 {
+		t.Fatalf("Invalid header on success page: %v", w.Result().StatusCode)
+	}
+	// check that the content has been properly parsed into the template
+	res := w.Body.String()
+	contentCheck := [...]string{
+		pDat.Session, pDat.AbstractNumber, pDat.Authors,
+		pDat.Title, pDat.Topic, pDat.ID, pDat.Abstract, 
+		"posterPath", "videoURL", "posterHash",
+		"supportEmail", "conferencePageURL",
+	}
+	containsAll := true
+	var missing string
+	for _, item := range contentCheck {
+		if !strings.Contains(res, item) {
+			containsAll = false
+			missing = fmt.Sprintf("%s %s", missing, item)
+		}
+	}
+	if !containsAll {
+		t.Fatalf("Success page content is missing: %s", missing)
+	}
+
+	// test parsing empty success page content
+	w = httptest.NewRecorder()
+	success(w, map[string]interface{}{})
+	if w.Result().StatusCode != 200 {
+		t.Fatalf("Invalid header on success page: %v", w.Result().StatusCode)
+	}
+
+	// test parsing invalid success page content
+	invalDat := map[string]interface{}{
+		"something":  "completely different",
+		"PosterHash": nil,
+	}
+	w = httptest.NewRecorder()
+	success(w, invalDat)
+	if w.Result().StatusCode != 200 {
+		t.Fatalf("Invalid header on success page: %v", w.Result().StatusCode)
 	}
 }
