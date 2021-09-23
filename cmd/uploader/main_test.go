fa2b9cda86c76d558eb1ba9f5fa06e7b5c3b52bb
M. Sonntag <michael.p.sonntag@gmail.com>
2021-09-23 16:13:53 +0200

    [test] Add saveFile test

--- cmd/uploader/main_test.go
+++ cmd/uploader/main_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
@@ -86,5 +89,50 @@ func TestRenameExistingFiles(t *testing.T) {
 	err = checkDirFiles(tmpDir, 2)
 	if err != nil {
 		t.Fatalf(err.Error())
+	}
+}
+
+func TestSaveFile(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "test_bc_save")
+	if err != nil {
+		t.Fatalf("Error creating tmp dir: %v", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	// prepare mock request file content
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	mockwriter, err := writer.CreateFormFile("test-file", "testfile.md")
+	if err != nil {
+		t.Fatalf("Error creating form file: %v", err)
+	}
+	filecontent := "[{'content':'test'}]"
+	_, err = mockwriter.Write([]byte(filecontent))
+	if err != nil {
+		t.Fatalf("Error writing file content: %v", err)
+	}
+	// yes, I am ignoring this error
+	_ = writer.Close()
+	req, _ := http.NewRequest("POST", "https://nowhere.com/", body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	// test saveFile with form file data
+	content, _, err := req.FormFile("test-file")
+	if err != nil {
+		t.Fatalf("Error fetching request content: %v", err)
+	}
+	topath := filepath.Join(tmpDir, "upload.md")
+	err = saveFile(content, topath)
+	if err != nil {
+		t.Fatalf("Error saving request content: %v", err)
+	}
+
+	// Check file content
+	cont, err := ioutil.ReadFile(topath)
+	if err != nil {
+		t.Fatalf("Error reading output file: %v", err)
+	}
+	if string(cont) != filecontent {
+		t.Fatalf("Unexpected file content: %s", cont)
 	}
 }
